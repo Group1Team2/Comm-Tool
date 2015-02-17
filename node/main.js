76598b0909f0aae2d5d47f643f7f93866d8e4026
Oscar Ingham <ojingham@gmail.com>
2015-02-17 18:26:53 -0500

    proof of concept working with MySQL

--- node/main.js
+++ node/main.js
@@ -4,17 +4,26 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 // Database stuff
-var mysql = require('node-mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'demo',
-	password: 'demo'
+	password: 'demo',
+	database: 'comm'
 });
 
 // Routes (for API)
 app.get('/', function(req, res) {
 	res.sendFile('index.html');
 });
+
+// Database test
+connection.connect();
+connection.query('select * from ChatUser;', function(err, rows, fields) {
+	if (err) throw err;
+	console.log(rows);
+});
+connection.end();
 
 // WebSocket stuff
 io.on('connection', function(socket) {
