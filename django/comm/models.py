3478a1f3597971accf16a9e24a083d202f451264
Oscar Ingham <ojingham@gmail.com>
2015-02-21 17:07:24 -0500

    updated models for iteration one

--- django/comm/models.py
+++ django/comm/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	name = models.CharField(max_length=50)
+	online = models.BooleanField(default=False)
+
+class Room(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.CharField(max_length=500)
+	public = models.BooleanField(default=True)
+
+class Message(models.Model):
+	text = models.TextField()
+	time = models.TimeField(auto_now=True)
+	room = models.ForeignKey(Room)
+	user = models.ForeignKey(User)
+	at_message = models.BooleanField(default=False)
