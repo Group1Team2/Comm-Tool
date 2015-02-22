eb68c6682d01fd7d36720a1fdb293139ac2ce034
Oscar Ingham <ojingham@gmail.com>
2015-02-22 16:41:37 -0500

    added pagination in DRF

--- django/group2/settings.py
+++ django/group2/settings.py
@@ -83,3 +83,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+### DJANGO REST FRAMEWORK STUFF
+
+REST_FRAMEWORK = {
+    'PAGINATE_BY': 10,                 # Default to 10
+    'PAGINATE_BY_PARAM': 'page_size',  # Allow client to override, using `?page_size=xxx`.
+    'MAX_PAGINATE_BY': 100             # Maximum limit allowed when using `?page_size=xxx`.
+}
