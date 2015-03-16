b40df0558553c99013593abc69fdc9aa6dd147ae
Oscar Ingham <yuyan91@bu.edu>
2015-03-15 23:44:01 -0400

    separate Javascript

---
+++ django/comm/static/function.js
@@ -0,0 +1,35 @@
+  var server_host = window.location.hostname;
+  var socket = io('http://' + server_host + ':3000');
+  var username = random_user();
+  // Get the messages from the API and add to the page
+  $.getJSON('http://' + server_host + ':8000/api/messages/?format=json', function(json) {
+    json.results.forEach(function(msg){
+      add_message(msg.text);
+    });
+  });
+  // When you receive a message, add it to the page
+  socket.on('msg', function(msg){
+    add_message(msg);
+  });
+  function add_message(msg) {
+    $('div#messagecontent').append(msg + '<br>');
+  }
+  // Called when button is clicked
+  function display() {
+    var message = username + " : " + document.getElementById("text").value;
+    socket.emit('msg', message);
+    $('input#text').val('');
+  }
+  // Generate random user
+  function random_user() {
+    var random_index = Math.floor( Math.random() * 100 ) + 1;
+    return "User " + random_index;
+  }
+  // Add a new message whenever the user presses the enter key
+  $(document).keypress(function(e) {
+      if(e.which == 13) {
+          display();
+      }
+  });
+
+
