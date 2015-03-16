8ca82396a0858825a200c189e123454d5f4b1e18
Oscar Ingham <yuyan91@bu.edu>
2015-03-15 23:41:03 -0400

    change the STATIC_ROOT in order to enable function.js i static folder

--- django/group2/settings.py
+++ django/group2/settings.py
@@ -83,7 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT='/home/demo/CS673/Comm-Tool/django/comm/static/style.css'
+STATIC_ROOT='/home/demo/CS673/Comm-Tool/django/comm/static/'
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR,"static"),
 )
