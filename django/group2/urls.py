f266d1829618f241d31aa4f614e5a8eeac9ad82e
xuxinzhu0081 <xxz@bu.edu>
2015-03-28 14:30:37 -0400

    urls.py

--- django/group2/urls.py
+++ django/group2/urls.py
@@ -1,15 +1,20 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from rest_framework import routers
 from comm import views
+from django.conf.urls import patterns, url, include
+from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 
-router = routers.DefaultRouter()
+router = SimpleRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'rooms', views.RoomViewSet)
 router.register(r'messages', views.MessageViewSet)
+router.register(r'messagedata', views.MessageDataViewSet)
+router.register(r'usertoroom', views.UserToRoomViewSet)
+urlpatterns=router.urls
 
-urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('comm.urls')),
-    url(r'^api/', include(router.urls)),
-)
+#urlpatterns = patterns('',
+#	url(r'^api/', include(router.urls)),
+#    url(r'^admin/', include(admin.site.urls)),
+#    url(r'^$', include('comm.urls')),   
+#) 
+
